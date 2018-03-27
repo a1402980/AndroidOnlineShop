e2c50ab876071ae9130810f3801554f2ab70c711
Ibrahim Beqiri <ibraa.cs@msn.com>
2018-03-27 09:31:32 +0200

    no message

--- app/src/main/java/com/androidonlineshop/androidonlineshop/activities/MainActivity.java
+++ app/src/main/java/com/androidonlineshop/androidonlineshop/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.androidonlineshop.androidonlineshop;
+package com.androidonlineshop.androidonlineshop.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.androidonlineshop.androidonlineshop.R;
 
 public class MainActivity extends AppCompatActivity {
 
