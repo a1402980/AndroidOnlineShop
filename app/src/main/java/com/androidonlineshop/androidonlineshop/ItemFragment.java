1427093d374a25013fb4244f9076bec82b4c3ede
Mikko Lerto <mikko.lerto@gmail.com>
2018-04-22 18:33:47 +0200

    item page

---
+++ app/src/main/java/com/androidonlineshop/androidonlineshop/ItemFragment.java
@@ -0,0 +1,44 @@
+package com.androidonlineshop.androidonlineshop;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ItemFragment extends Fragment {
+
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    // TODO: Rename and change types and number of parameters
+    public static ItemFragment newInstance() {
+        ItemFragment fragment = new ItemFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item, container, false);
+    }
+
+
+}
