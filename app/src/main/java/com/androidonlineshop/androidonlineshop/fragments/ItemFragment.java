a2d81efc63e1287011b27b6f3ea409136cbc0642
Mikko Lerto <mikko.lerto@gmail.com>
2018-04-24 08:42:18 +0200

    item page and categories in sell item

--- app/src/main/java/com/androidonlineshop/androidonlineshop/fragments/ItemFragment.java
+++ app/src/main/java/com/androidonlineshop/androidonlineshop/fragments/ItemFragment.java
@@ -1,12 +1,12 @@
-package com.androidonlineshop.androidonlineshop;
+package com.androidonlineshop.androidonlineshop.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.androidonlineshop.androidonlineshop.R;
 
 
 public class ItemFragment extends Fragment {
