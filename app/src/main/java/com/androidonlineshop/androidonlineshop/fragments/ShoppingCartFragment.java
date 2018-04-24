4cf582971a6d8440b4ec4d064e91970a534a052e
Mikko Lerto <mikko.lerto@gmail.com>
2018-04-24 20:28:57 +0200

    crashing line commented

--- app/src/main/java/com/androidonlineshop/androidonlineshop/fragments/ShoppingCartFragment.java
+++ app/src/main/java/com/androidonlineshop/androidonlineshop/fragments/ShoppingCartFragment.java
@@ -63,7 +63,9 @@ public class ShoppingCartFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_shopping_cart, container, false);
 
-        cartItems = view.findViewById(R.id.cartItems);
+
+        //THIS LINE CAUSES HELL UPON MY EMULATOR
+        //cartItems = view.findViewById(R.id.cartItems);
 
         return view;
     }
